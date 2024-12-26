fdc6852d7d534ab9f8e44472d3d918f4654c4591
Vijay Shankar <vijay.shankar@msci.com>
2024-12-26 15:50:11 +0000

    modus quick start

--- main.go
+++ main.go
@@ -17,3 +17,39 @@ func SayHello(name *string) string {
 
 	return fmt.Sprintf("Hello, %s!", s)
 }
+
+package main
+
+import (
+  "errors"
+  "fmt"
+
+  "github.com/hypermodeinc/modus/sdk/go/pkg/http"
+)
+
+type Quote struct {
+  Quote  string `json:"q"`
+  Author string `json:"a"`
+}
+
+// this function makes a request to an API that returns data in JSON format, and
+// returns an object representing the data
+func GetRandomQuote() (*Quote, error) {
+  request := http.NewRequest("https://zenquotes.io/api/random")
+
+  response, err := http.Fetch(request)
+  if err != nil {
+    return nil, err
+  }
+  if !response.Ok() {
+    return nil, fmt.Errorf("Failed to fetch quote. Received: %d %s", response.Status, response.StatusText)
+  }
+
+  // the API returns an array of quotes, but we only want the first one
+  var quotes []Quote
+  response.JSON(&quotes)
+  if len(quotes) == 0 {
+    return nil, errors.New("expected at least one quote in the response, but none were found")
+  }
+  return &quotes[0], nil
+}
