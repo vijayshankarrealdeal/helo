e9e2409f4f4706264c5061a77387b9de83cf90af
Vijay Shankar <54475842+vijayshankarrealdeal@users.noreply.github.com>
2025-01-27 14:41:08 +0000

    changed env urls

--- s_api.py
+++ s_api.py
@@ -3,4 +3,6 @@ from supabase import create_client, Client
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+def get_client():
+    supabase: Client = create_client(url, key)
+    return supabase
